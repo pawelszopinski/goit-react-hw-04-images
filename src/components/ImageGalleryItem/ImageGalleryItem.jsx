e36ef64ead50206b3297e11c5b88e954e56b5f74
Paweł Szopiński <szopinskipawel@gmail.com>
2023-07-16 23:32:14 +0200

    updated fetch
    added pixabay request

--- src/components/ImageGalleryItem/ImageGalleryItem.jsx
+++ src/components/ImageGalleryItem/ImageGalleryItem.jsx
@@ -6,15 +6,15 @@ function ImageGalleryItem() {
     console.log(image)
     useEffect(() => {
         async function getImages() {
-            const res = await fetch("https://api.imgflip.com/get_memes")
+            const res = await fetch("https://pixabay.com/api/?key=36237308-42aa754ef31b34db7b4fcf11d&q=yellow+flowers&image_type=photo&pretty=true")
             const data = await res.json()
-            setImage(data.data.memes)
+            setImage(data.hits)
         }
         getImages()
     }, [])
-  return (
-<li className="gallery-item">
-  <img className='gallery-item-image' src="https://plus.unsplash.com/premium_photo-1688114984765-308599ec1e13?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwyfHx8ZW58MHx8fHx8&auto=format&fit=crop&w=600&q=60" alt="" />
-</li>)
-}
+    
+  return ( <pre>{JSON.stringify(image, null, 2)}</pre>
+
+  )
+  }
 export default ImageGalleryItem
